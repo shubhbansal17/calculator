984ce232981d5debc5c0e731c7ff2b948754dad8
Shubhbansal17 <shubhb487@gmail.com>
2026-05-31 17:26:55 +0530

    Initial calculator project

---
+++ README.md
@@ -0,0 +1,24 @@
+# Calculator
+
+A simple calculator built using Python.
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## How to Run
+
+1. Clone the repository
+2. Open terminal
+3. Run:
+
+```bash
+python3 main.py
+```
+
+## Author
+
+Shubh Bansal
---
+++ calc.py
@@ -0,0 +1,12 @@
+#this is a calculator made in python
+while True:
+    num1=int(input('enter number 1'))
+    num2=int(input('enter nmber2'))
+    oper=input('enter operation you want to perform')
+    if oper=='add':
+        print(num1+num2)
+    elif oper=='subtract':
+        print(num1-num2)
+    ans=input('do you want to do more operations')
+    if ans in 'nN':
+        break
